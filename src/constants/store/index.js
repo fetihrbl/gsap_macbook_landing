1cc6f5f35608253f50b25430ec1a3bd9bef602e2
Fatih Erbil <fatiherbil@Fatihs-MacBook-Air.local>
2026-05-11 05:56:12 -0700

    implement features section

--- src/constants/store/index.js
+++ src/constants/store/index.js
@@ -8,7 +8,10 @@ const useMacbookStore = create((set) => ({
     scale: 0.08,
     setScale: (scale) => set({scale}),
 
-    reset: () => set({color: '#2e2e3c', scale: 0.08}),
+    texture: '/videos/feature-1.mp4',
+    setTexture: (texture) => set({ texture }),
+
+    reset: () => set({color: '#2e2e2c', scale: 0.08, texture: '/videos/feature-1.mp4'}),
 
 }))
 
